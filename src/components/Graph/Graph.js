45236eb56c8c5ebf47df9ac79287d4eb6bbc58b7
Lam Nguyen <lnguye16@gustavus.edu>
2018-06-21 10:17:50 -0500

    make it better

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -20,8 +20,7 @@ class Graph extends Component{
 	                'rgba(75, 192, 192, 0.2)',
 	                'rgba(153, 102, 255, 0.2)',
 	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
+	            ],borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
